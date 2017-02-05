8057456d26bb8f96ab8ced36769a80cdb59ffb38
Tom Elliff <tomelliff@gmail.com>
2017-02-05 23:03:47 +0000

    Add logging permissions for the Lambda function

--- s3-sftp-bridge/lambda.tf
+++ s3-sftp-bridge/lambda.tf
@@ -42,10 +42,33 @@ resource "aws_iam_role_policy" "lambda_s3_access" {
 EOF
 }
 
+resource "aws_iam_role_policy" "lambda_logging" {
+  role = "${aws_iam_role.iam_for_lambda.id}"
+  name = "logging"
+
+  policy = <<EOF
+{
+  "Version"  : "2012-10-17",
+  "Statement": [
+    {
+      "Sid"     :   "1",
+      "Effect"  :   "Allow",
+
+      "Action"  : [ "logs:CreateLogGroup",
+                    "logs:CreateLogStream",
+                    "logs:PutLogEvents" ],
+      "Resource":   "arn:aws:logs:*:*:*"
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_lambda_function" "s3_sftp_bridge_lambda" {
   s3_bucket     = "${var.lambda_s3_bucket}"
   s3_key        = "${var.lambda_s3_key}"
   function_name = "s3-sftp-bridge-${var.integration_name}"
+  description   = "Used to sync files between S3 and SFTP servers"
   runtime       = "nodejs4.3"
   role          = "${aws_iam_role.iam_for_lambda.arn}"
   handler       = "exports.handle"
